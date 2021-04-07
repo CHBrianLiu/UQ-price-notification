e3521293864b92df6c46270f722755daf2b9f454
Brian Liu <chbrian.liu@outlook.com>
2021-04-08 00:14:44 +0800

    Add logger initialization

--- app/main.py
+++ app/main.py
@@ -1,7 +1,22 @@
+from app.config.loader import get_config_by_key
+import logging
 from fastapi import FastAPI
 
 app = FastAPI()
+logger = logging.getLogger()
 
+log_level = get_config_by_key("logging.level")
+if log_level == "critical":
+    logger.setLevel(logging.CRITICAL)
+elif log_level == "warning":
+    logger.setLevel(logging.WARNING)
+elif log_level == "info":
+    logger.setLevel(logging.INFO)
+elif log_level == "debug":
+    logger.setLevel(logging.DEBUG)
+else:
+    # Default is error
+    logger.setLevel(logging.ERROR)
 
 @app.get("/")
 async def root():
