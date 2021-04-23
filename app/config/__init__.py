70fad5b86d73eea9bde279b2379cdaa8bbf68779
Brian Liu <chbrian.liu@outlook.com>
2021-04-24 01:01:25 +0800

    New config loader.

---
+++ app/config/__init__.py
@@ -0,0 +1,24 @@
+import json
+import dotenv
+import os
+
+from .config_model import AppConfig
+
+DEFAULT_ENV_PATH = os.path.join(os.path.dirname(__file__), "../../default.env")
+LOCAL_ENV_PATH = os.path.join(os.path.dirname(__file__), "../../local.env")
+
+loaded_config = {
+    **dotenv.dotenv_values(DEFAULT_ENV_PATH),
+    **dotenv.dotenv_values(LOCAL_ENV_PATH),
+}
+
+# manual conversion
+loaded_config["AZURE_BLOB_CONTAINERS"] = json.loads(
+    loaded_config["AZURE_BLOB_CONTAINERS"]
+)
+loaded_config["UQ_ON_SALE_ICON_CSS_LIST"] = json.loads(
+    loaded_config["UQ_ON_SALE_ICON_CSS_LIST"]
+)
+
+# USE THIS AS CONFIG OBJECT
+app_config = AppConfig(**loaded_config)
---
+++ app/config/config_model.py
@@ -0,0 +1,31 @@
+from typing import List
+from pydantic import BaseModel
+
+
+class AppConfig(BaseModel):
+    # Debug mode
+    LOCAL_TESTING: bool
+    # LINE ENDPOINTS
+    LINE_REPLY_ENDPOINT: str
+    LINE_PUSH_ENDPOINT: str
+    LINE_LINE_BOT_CHANNEL_SECRET: str
+    LINE_LINE_BOT_CHANNEL_TOKEN: str
+
+    # LOGGING
+    LOGGING_LEVEL: str
+
+    # AZURE
+    AZURE_BLOB_CONTAINERS: List[str]
+    AZURE_ACOUNT_NAME: str
+    AZURE_ACOUNT_KEY: str
+
+    # UQ
+    UQ_PRODUCT_URL_PREFIX: str
+    UQ_PRODUCT_NAME_CSS: str
+    UQ_ICON_LIST_CSS: str
+    UQ_ON_SALE_ICON_CSS_LIST: List[str]
+    UQ_HIDE_ICON_STYLE: str
+
+    # CRON
+    CRON_PRICE_CHECKING: str
+    CRON_NOTIFICATION_PUSHING: str
