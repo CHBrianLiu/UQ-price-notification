ca3953d45503d79319cbc8c39b674c696f50af2c
Brian Liu <chbrian.liu@outlook.com>
2021-04-25 23:26:25 +0800

    Fix inexisting property issue.

--- app/cron/callback_job.py
+++ app/cron/callback_job.py
@@ -8,4 +8,4 @@ def callback():
     logging.info(f"Call {app_config.AZURE_WEB_APP_CALLBACK_URL} to keep app awake.")
     with requests.get(app_config.AZURE_WEB_APP_CALLBACK_URL) as response:
         if not response.ok:
-            logging.error("Call back error with code %s.", response.status)
+            logging.error("Call back error with code %s.", response.status_code)
