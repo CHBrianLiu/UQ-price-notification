e1f0bc807513fc7654f1a0ed3dd5542eedc7b380
Brian Liu <chbrian.liu@outlook.com>
2021-04-27 00:32:06 +0800

    Adjust callback failure log level

--- app/cron/callback_job.py
+++ app/cron/callback_job.py
@@ -8,4 +8,9 @@ def callback():
     logging.info(f"Call {app_config.AZURE_WEB_APP_CALLBACK_URL} to keep app awake.")
     with requests.get(app_config.AZURE_WEB_APP_CALLBACK_URL) as response:
         if not response.ok:
-            logging.error("Call back error with code %s.", response.status_code)
+            logging.warning("Call back error with code %s.", response.status_code)
+            if response.status_code == 403:
+                logging.debug(
+                    "x-ms-forbidden-ip: %s",
+                    response.headers.get("x-ms-forbidden-ip", ""),
+                )
