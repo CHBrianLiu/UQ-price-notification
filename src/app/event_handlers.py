8904f44c0d3a93d60bfe2b192db3e6545af4734d
brianliu <hong86323@gmail.com>
2022-02-06 17:20:01 +0800

    re-construct the webhook handling flow. - Move the uq url parser to uq utils folder. - Take care of dependency tree.

--- src/app/event_handlers.py
+++ src/app/event_handlers.py
@@ -1,28 +1,27 @@
+import linebot
 from linebot.models import MessageEvent, TextMessage, PostbackEvent
 
-from src.app.routes import webhook_handler
-from src.shared.line.postback_action_models import ProductRemovalPostbackDataModel
+from src.shared.uq import uq_url_utils
+from src.shared.line import plain_messages
+from src import config
+
+line_api = linebot.LineBotApi(config.LINE_CHANNEL_TOKEN, config.LINE_API_ENDPOINT)
+webhook_handler = linebot.WebhookHandler(config.LINE_CHANNEL_SECRET)
 
 
 @webhook_handler.add(MessageEvent, message=TextMessage)
 def handle_text_message_event(event: MessageEvent):
     content: str = event.message.text
+    reply_token = event.reply_token
     # check url validity.
-    product_code = get_product_code_from_mobile_url(content) or get_product_code_from_desktop_url(content)
+    product_code = uq_url_utils.UqProductCodeParser(content).get_product_code_from_url()
     if product_code:
         return
     # default response
+    message = plain_messages.HelpMessageCreator().generate_help_message()
+    line_api.reply_message(reply_token, message)
+
     return
-
-
-def get_product_code_from_mobile_url(url: str) -> str | None:
-    # https://m.uniqlo.com/tw/product?pid=u0000000009993
-    pass
-
-
-def get_product_code_from_desktop_url(url: str) -> str | None:
-    # https://www.uniqlo.com/tw/zh_TW/product-detail.html?productCode=u0000000009993
-    pass
 
 
 @webhook_handler.add(PostbackEvent)
--- src/app/routes.py
+++ src/app/routes.py
@@ -2,18 +2,19 @@ import fastapi
 import linebot
 import linebot.exceptions
 
-from src import config
+from src.app.event_handlers import webhook_handler
 
 router = fastapi.APIRouter()
-webhook_handler = linebot.WebhookHandler(config.LINE_CHANNEL_SECRET)
 
 
 @router.post("/webhook")
-async def line_message_api_webhook_api(body: str = fastapi.Body(...), x_line_signature: str = fastapi.Header(...)):
+async def line_message_api_webhook_api(request: fastapi.Request):
     """
     Respond 200 OK for every request that passes the signature validation
     """
     try:
+        body = (await request.body()).decode("utf-8")
+        x_line_signature = request.headers.get("x-line-signature", "")
         webhook_handler.handle(body, x_line_signature)
     except linebot.exceptions.InvalidSignatureError as e:
         raise fastapi.HTTPException(400) from e
