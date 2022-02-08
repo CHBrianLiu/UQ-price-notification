60872721ce030e13c5af37ffca338a1cbba71cd1
brianliu <hong86323@gmail.com>
2022-02-08 17:35:26 +0800

    Add product code error message creator class

--- src/shared/line/plain_messages.py
+++ src/shared/line/plain_messages.py
@@ -9,3 +9,8 @@ class HelpMessageCreator:
 class PriceDownNotificationMessageCreator:
     def generate_price_down_notification_message(self):
         return models.TextMessage(text="你好，你追蹤的商品正在特價中！把握機會購買吧！")
+
+
+class ProductUrlErrorMessageCreator:
+    def generate_product_url_error_message(self):
+        return models.TextMessage(text="你所輸入的商品網址有誤，請重新輸入！")
