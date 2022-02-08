5e636bcaaedf09f4895e6ba46589db86ba9f094a
Chunhung (Brian) Liu <hong86323@gmail.com>
2022-02-08 22:53:50 +0800

    [Line] Product card content modification (+ refactoring) (#13)
    * Revise the product card content.
    
    * Refactoring: split UqProductManagementTemplateMessageCreator class to separate duties.

--- src/shared/line/product_management.py
+++ src/shared/line/product_management.py
@@ -4,41 +4,28 @@ from src.shared.uq.uq_product import UqProduct
 from src.shared.line.postback_action_models import ProductRemovalPostbackDataModel
 
 
-class UqProductManagementTemplateMessageCreator:
-    """
-    Class for creating the carousel template message for user product management.
-    """
+class UqProductManagementTemplateColumnCreator:
+    _product: UqProduct
 
-    _uq_products: list[UqProduct]
+    def __init__(self, product: UqProduct):
+        self._product = product
 
-    def __init__(self, products: list[UqProduct]):
-        # Decided not to use DI here because we heavily rely on data models.
-        self._uq_products = products
-
-    def generate_products_carousel_template_message(self):
-        carousel_template_columns = [
-            self._generate_product_carousel_column(product)
-            for product in self._uq_products
-        ]
-        template = models.CarouselTemplate(columns=carousel_template_columns)
-        return models.TemplateSendMessage(template=template)
-
-    def _generate_product_carousel_column(self, product: UqProduct):
-        card_title = product.name
-        card_text = f"NT ${product.special_offer}"
-        go_website_button = self._generate_product_go_website_action(product)
-        removal_button = self._generate_product_removal_action(product)
+    def generate_product_carousel_column(self):
+        card_title = self._product.name
+        card_text = self._compose_card_content()
+        go_website_button = self._generate_product_go_website_action()
+        removal_button = self._generate_product_removal_action()
         return models.CarouselColumn(
             title=card_title,
             text=card_text,
             actions=[go_website_button, removal_button],
         )
 
-    def _generate_product_removal_action(self, product: UqProduct):
+    def _generate_product_removal_action(self):
         action_button_label = "取消追蹤"
-        action_button_click_message = f"取消追蹤 {product.name}"
+        action_button_click_message = f"取消追蹤 {self._product.name}"
         postback_data = ProductRemovalPostbackDataModel(
-            product_code=product.product_code
+            product_code=self._product.product_code
         ).json()
         return models.PostbackAction(
             display_text=action_button_click_message,
@@ -46,7 +33,47 @@ class UqProductManagementTemplateMessageCreator:
             data=postback_data,
         )
 
-    def _generate_product_go_website_action(self, product: UqProduct):
+    def _generate_product_go_website_action(self):
         action_button_label = "前往官網"
-        action_button_url = product.website_url
+        action_button_url = self._product.website_url
         return models.URIAction(label=action_button_label, uri=action_button_url)
+
+    def _compose_card_content(self):
+        if self._product.is_on_sale:
+            return f"原價：NT$ {self._product.original_price}\n特價：NT$ {self._product.special_offer}"
+        return f"NT$ {self._product.original_price}"
+
+
+class UqProductManagementTemplateFactory:
+    def get_uq_product_management_template_column_creator(self, *args, **kwargs):
+        return UqProductManagementTemplateColumnCreator(*args, **kwargs)
+
+
+class UqProductManagementTemplateMessageCreator:
+    """
+    Class for creating the carousel template message for user product management.
+    """
+
+    _uq_products: list[UqProduct]
+
+    def __init__(
+        self,
+        products: list[UqProduct],
+        factory: UqProductManagementTemplateFactory = None,
+    ):
+        # Decided not to use DI here because we heavily rely on data models.
+        self._uq_products = products
+        self._factory = (
+            factory if factory is not None else UqProductManagementTemplateFactory()
+        )
+
+    def generate_products_carousel_template_message(self):
+        carousel_template_columns = []
+        for product in self._uq_products:
+            column_creator = (
+                self._factory.get_uq_product_management_template_column_creator(product)
+            )
+            column = column_creator.generate_product_carousel_column()
+            carousel_template_columns.append(column)
+        template = models.CarouselTemplate(columns=carousel_template_columns)
+        return models.TemplateSendMessage(template=template)
--- tests/ut/shared/line/test_product_management.py
+++ tests/ut/shared/line/test_product_management.py
@@ -4,23 +4,116 @@ from unittest import mock
 from linebot import models
 
 from src.shared.line.postback_action_models import ProductRemovalPostbackDataModel
-from src.shared.line.product_management import UqProductManagementTemplateMessageCreator
+from src.shared.line.product_management import (
+    UqProductManagementTemplateMessageCreator,
+    UqProductManagementTemplateColumnCreator,
+)
+
+
+class TestUqProductManagementTemplateColumnCreator(unittest.TestCase):
+    def test_products_carousel_template_column_title_should_be_product_name(self):
+        product = mock.MagicMock()
+        product.name = "product"
+        product.product_code = "product_code"
+        product.website_url = "https://abcd.com/product"
+        product.is_on_sale = False
+        product.original_price = 200
+
+        creator = UqProductManagementTemplateColumnCreator(product)
+        column = creator.generate_product_carousel_column()
+
+        self.assertEquals("product", column.title)
+
+    def test_products_carousel_template_column_content_should_use_corrent_format_if_product_is_on_sale(
+        self,
+    ):
+        product = mock.MagicMock()
+        product.name = "product"
+        product.product_code = "product_code"
+        product.website_url = "https://abcd.com/product"
+        product.is_on_sale = True
+        product.original_price = 200
+        product.special_offer = 100
+
+        creator = UqProductManagementTemplateColumnCreator(product)
+        column = creator.generate_product_carousel_column()
+
+        self.assertEquals("原價：NT$ 200\n特價：NT$ 100", column.text)
+
+    def test_products_carousel_template_column_should_contain_go_website_and_removal_actions(
+        self,
+    ):
+        product = mock.MagicMock()
+        product.name = "product"
+        product.product_code = "product_code"
+        product.website_url = "https://abcd.com/product1"
+        product.special_offer = 100
+
+        creator = UqProductManagementTemplateColumnCreator(product)
+        column = creator.generate_product_carousel_column()
+        go_website_button = column.actions[0]
+        removal_button = column.actions[1]
+
+        self.assertIsInstance(go_website_button, models.URIAction)
+        self.assertIsInstance(removal_button, models.PostbackAction)
+
+    def test_products_carousel_template_column_go_website_button_link_should_be_product_website(
+        self,
+    ):
+        product = mock.MagicMock()
+        product.name = "product"
+        product.product_code = "product_code"
+        product.website_url = "https://abcd.com/product1"
+        product.special_offer = 100
+
+        creator = UqProductManagementTemplateColumnCreator(product)
+        column = creator.generate_product_carousel_column()
+        go_website_button = column.actions[0]
+
+        self.assertEquals(product.website_url, go_website_button.uri)
+
+    def test_products_carousel_template_column_removal_button_link_should_contain_correct_data(
+        self,
+    ):
+        product = mock.MagicMock()
+        product.name = "product"
+        product.product_code = "product_code"
+        product.website_url = "https://abcd.com/product"
+        product.special_offer = 100
+
+        creator = UqProductManagementTemplateColumnCreator(product)
+        column = creator.generate_product_carousel_column()
+        removal_button = column.actions[1]
+        removal_postback_data = ProductRemovalPostbackDataModel.parse_raw(
+            removal_button.data
+        )
+
+        self.assertEquals("remove", removal_postback_data.action)
+        self.assertEquals("product_code", removal_postback_data.product_code)
 
 
 class TestUqProductManagementTemplateMessageCreator(unittest.TestCase):
-    def test_generate_products_carousel_template_should_contain_products_info_in_column(
+    """
+    We don't mock the factory class and UqProductManagementTemplateColumnCreator
+    because the flow is relatively simple. By testing with real dependencies, we
+    can catch the bug earlier.
+    """
+
+    def test_generated_carousel_template_message_should_contain_product_columns(
         self,
     ):
         product1 = mock.MagicMock()
         product1.name = "product1"
         product1.product_code = "product_code1"
         product1.website_url = "https://abcd.com/product1"
-        product1.special_offer = 100
+        product1.is_on_sale = False
+        product1.original_price = 100
         product2 = mock.MagicMock()
         product2.name = "product2"
         product2.product_code = "product_code2"
         product2.website_url = "https://abcd.com/product2"
-        product2.special_offer = 200
+        product2.is_on_sale = False
+        product2.original_price = 200
 
         creator = UqProductManagementTemplateMessageCreator([product1, product2])
         message = creator.generate_products_carousel_template_message()
@@ -29,60 +122,4 @@ class TestUqProductManagementTemplateMessageCreator(unittest.TestCase):
 
         self.assertEquals(2, len(message.template.columns))
         self.assertEquals("product1", column1.title)
-        self.assertEquals("NT $100", column1.text)
         self.assertEquals("product2", column2.title)
-        self.assertEquals("NT $200", column2.text)
-
-    def test_products_carousel_template_column_should_contain_go_website_and_removal_actions(
-        self,
-    ):
-        product1 = mock.MagicMock()
-        product1.name = "product1"
-        product1.product_code = "product_code1"
-        product1.website_url = "https://abcd.com/product1"
-        product1.special_offer = 100
-
-        creator = UqProductManagementTemplateMessageCreator([product1])
-        message = creator.generate_products_carousel_template_message()
-        column1 = message.template.columns[0]
-        go_website_button = column1.actions[0]
-        removal_button = column1.actions[1]
-
-        self.assertIsInstance(go_website_button, models.URIAction)
-        self.assertIsInstance(removal_button, models.PostbackAction)
-
-    def test_products_carousel_template_column_go_website_button_link_should_be_product_website(
-        self,
-    ):
-        product1 = mock.MagicMock()
-        product1.name = "product1"
-        product1.product_code = "product_code1"
-        product1.website_url = "https://abcd.com/product1"
-        product1.special_offer = 100
-
-        creator = UqProductManagementTemplateMessageCreator([product1])
-        message = creator.generate_products_carousel_template_message()
-        column1 = message.template.columns[0]
-        go_website_button = column1.actions[0]
-
-        self.assertEquals(product1.website_url, go_website_button.uri)
-
-    def test_products_carousel_template_column_removal_button_link_should_contain_correct_data(
-        self,
-    ):
-        product1 = mock.MagicMock()
-        product1.name = "product1"
-        product1.product_code = "product_code1"
-        product1.website_url = "https://abcd.com/product1"
-        product1.special_offer = 100
-
-        creator = UqProductManagementTemplateMessageCreator([product1])
-        message = creator.generate_products_carousel_template_message()
-        column1 = message.template.columns[0]
-        removal_button = column1.actions[1]
-        removal_postback_data = ProductRemovalPostbackDataModel.parse_raw(
-            removal_button.data
-        )
-
-        self.assertEquals("remove", removal_postback_data.action)
-        self.assertEquals("product_code1", removal_postback_data.product_code)
