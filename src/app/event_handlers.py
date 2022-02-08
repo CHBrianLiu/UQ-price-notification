440a26e205e43f3fe31bcdda9672ad8d18c9d7c9
brianliu <hong86323@gmail.com>
2022-02-08 17:36:30 +0800

    [Draft] Add product subscription message handler
    Todo items
    - Make code more testable. especially `process_product_subscription_attempt` function.

--- src/app/event_handlers.py
+++ src/app/event_handlers.py
@@ -1,8 +1,9 @@
 import linebot
 from linebot.models import MessageEvent, TextMessage, PostbackEvent
+import requests
 
-from src.shared.uq import uq_url_utils
-from src.shared.line import plain_messages
+from src.shared.uq import uq_url_utils, uq_product
+from src.shared.line import plain_messages, product_subscription
 from src import config
 
 line_api = linebot.LineBotApi(config.LINE_CHANNEL_TOKEN, config.LINE_API_ENDPOINT)
@@ -16,12 +17,27 @@ def handle_text_message_event(event: MessageEvent):
     # check url validity.
     product_code = uq_url_utils.UqProductCodeParser(content).get_product_code_from_url()
     if product_code:
+        process_product_subscription_attempt(product_code, reply_token)
         return
     # default response
     message = plain_messages.HelpMessageCreator().generate_help_message()
     line_api.reply_message(reply_token, message)
 
     return
+
+
+def process_product_subscription_attempt(product_code: str, reply_token: str):
+    try:
+        with requests.Session() as session:
+            uq_retriever = uq_product.UqRetriever(product_code, session)
+            product = uq_product.UqProduct(uq_retriever)
+            creator = product_subscription.UqProductSubscriptionConfirmationMessageCreator(product)
+            message = creator.generate_product_subscription_confirmation_message()
+
+    except uq_product.UqProductException:
+        message = plain_messages.ProductUrlErrorMessageCreator().generate_product_url_error_message()
+
+    line_api.reply_message(reply_token, message)
 
 
 @webhook_handler.add(PostbackEvent)
