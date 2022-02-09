8b90f79cd25fcdc6c94ad37f9f5e7d6f6256c752
brianliu <hong86323@gmail.com>
2022-02-09 15:24:22 +0800

    Add the default reply message flow.

--- src/app/event_handlers.py
+++ src/app/event_handlers.py
@@ -1,6 +1,7 @@
 import linebot
 from linebot.models import MessageEvent, TextMessage, PostbackEvent
 
+from src.shared.line.message_creators import HelpMessageCreator
 from src import config
 
 line_api = linebot.LineBotApi(config.LINE_CHANNEL_TOKEN, config.LINE_API_ENDPOINT)
@@ -9,11 +10,14 @@ webhook_handler = linebot.WebhookHandler(config.LINE_CHANNEL_SECRET)
 
 @webhook_handler.add(MessageEvent, message=TextMessage)
 def handle_text_message_event(event: MessageEvent):
-    # TODO: Text message event handling logics
-    pass
+    reply_token = event.reply_token
+
+    # default response
+    message = HelpMessageCreator().generate()
+    line_api.reply_message(reply_token, message)
 
 
 @webhook_handler.add(PostbackEvent)
-def handle_postback_event(event: PostbackEvent):
-    # TODO: Postback event handling logic
+def handle_postback_event(event: PostbackEvent):  # pylint: disable=W0613
+    # TODO: Postback event handling logic  pylint: disable=W0511
     pass
