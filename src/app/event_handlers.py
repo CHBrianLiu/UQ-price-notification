6620af4083b4c19c80c16e622f0f6a574b1805b9
brianliu <hong86323@gmail.com>
2022-02-13 17:50:37 +0800

    Add product subscription logic to text message handler.

--- src/app/event_handlers.py
+++ src/app/event_handlers.py
@@ -1,8 +1,16 @@
 import linebot
 from linebot.models import MessageEvent, TextMessage, PostbackEvent
+import requests
 
-from src.shared.line.message_creators import HelpMessageCreator
+from src.shared.line.message_creators import (
+    HelpMessageCreator,
+    LineMessageCreator,
+    UqProductSubscriptionConfirmationMessageCreator,
+    ProductUrlErrorMessageCreator,
+)
 from src import config
+from src.shared.uq.uq_url_utils import UqProductCodeParser
+from src.shared.uq import uq_product
 
 line_api = linebot.LineBotApi(config.LINE_CHANNEL_TOKEN, config.LINE_API_ENDPOINT)
 webhook_handler = linebot.WebhookHandler(config.LINE_CHANNEL_SECRET)
@@ -11,10 +19,29 @@ webhook_handler = linebot.WebhookHandler(config.LINE_CHANNEL_SECRET)
 @webhook_handler.add(MessageEvent, message=TextMessage)
 def handle_text_message_event(event: MessageEvent):
     reply_token = event.reply_token
+    message = event.message.text
 
-    # default response
-    message = HelpMessageCreator().generate()
-    line_api.reply_message(reply_token, message)
+    if product_code := UqProductCodeParser(message).get_product_code_from_url():
+        reply_message = generate_response_for_given_product_code(product_code)
+    else:
+        reply_message = HelpMessageCreator().generate()
+
+    line_api.reply_message(reply_token, reply_message)
+
+
+def generate_response_for_given_product_code(product_code: str) -> LineMessageCreator:
+    try:
+        with requests.Session() as session:
+            retriever = uq_product.UqRetriever(product_code, session)
+            product = uq_product.UqProduct(retriever)
+            message = UqProductSubscriptionConfirmationMessageCreator(
+                product
+            ).generate()
+
+    except uq_product.UqProductException:
+        message = ProductUrlErrorMessageCreator().generate()
+
+    return message
 
 
 @webhook_handler.add(PostbackEvent)
