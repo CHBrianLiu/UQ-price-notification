3d0855190b7e83b189371ea5c41ceb97c56e921d
Chunhung (Brian) Liu <hong86323@gmail.com>
2022-02-13 16:35:20 +0800

    Add webhook handlers backbones. (#14)

--- src/app/routes.py
+++ src/app/routes.py
@@ -1,20 +1,23 @@
 import fastapi
+import linebot
+import linebot.exceptions
 
-from src.app import api_validators
+from src.app.event_handlers import webhook_handler
 
 router = fastapi.APIRouter()
 
 
-@router.post(
-    "/webhook",
-    dependencies=[
-        fastapi.Depends(api_validators.line_webhook_request_signature_validator)
-    ],
-)
-async def line_message_api_webhook_api():
+@router.post("/webhook")
+async def line_message_api_webhook_api(request: fastapi.Request):
     """
     Respond 200 OK for every request that passes the signature validation
     """
+    try:
+        body = (await request.body()).decode("utf-8")
+        x_line_signature = request.headers.get("x-line-signature", "")
+        webhook_handler.handle(body, x_line_signature)
+    except linebot.exceptions.InvalidSignatureError as e:
+        raise fastapi.HTTPException(400) from e
     return
 
 
