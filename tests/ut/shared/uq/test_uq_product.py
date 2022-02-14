51f3c799ba50c6e018c22742c2a0453b2700b4ff
Brian Hu <b159951519@gmail.com>
2022-02-14 15:24:16 +0800

    Retrive image url from image info API

--- tests/ut/shared/uq/test_uq_product.py
+++ tests/ut/shared/uq/test_uq_product.py
@@ -180,6 +180,139 @@ class MockUqRetriever:
             ],
         }
 
+    def get_image_info(self):
+        return {
+            "mStorePic": ["/hmall/test/u0000000000242/storeDescription/1.jpg"],
+            "video": {},
+            "rows": {
+                "u0000000000242006": {
+                    "skuPic_40": "/hmall/test/u0000000000242/sku/40/COL69.jpg",
+                    "skuPic_1000": "/hmall/test/u0000000000242/sku/1000/COL69.jpg",
+                    "chipPic_22": "/hmall/test/u0000000000242/chip/22/COL69.jpg",
+                    "skuPic_285": "/hmall/test/u0000000000242/sku/285/COL69.jpg",
+                    "skuPic_561": "/hmall/test/u0000000000242/sku/561/COL69.jpg",
+                },
+                "u0000000000242005": {
+                    "skuPic_40": "/hmall/test/u0000000000242/sku/40/COL69.jpg",
+                    "skuPic_1000": "/hmall/test/u0000000000242/sku/1000/COL69.jpg",
+                    "chipPic_22": "/hmall/test/u0000000000242/chip/22/COL69.jpg",
+                    "skuPic_285": "/hmall/test/u0000000000242/sku/285/COL69.jpg",
+                    "skuPic_561": "/hmall/test/u0000000000242/sku/561/COL69.jpg",
+                },
+                "u0000000000242004": {
+                    "skuPic_40": "/hmall/test/u0000000000242/sku/40/COL69.jpg",
+                    "skuPic_1000": "/hmall/test/u0000000000242/sku/1000/COL69.jpg",
+                    "chipPic_22": "/hmall/test/u0000000000242/chip/22/COL69.jpg",
+                    "skuPic_285": "/hmall/test/u0000000000242/sku/285/COL69.jpg",
+                    "skuPic_561": "/hmall/test/u0000000000242/sku/561/COL69.jpg",
+                },
+                "u0000000000242003": {
+                    "skuPic_40": "/hmall/test/u0000000000242/sku/40/COL66.jpg",
+                    "skuPic_1000": "/hmall/test/u0000000000242/sku/1000/COL66.jpg",
+                    "chipPic_22": "/hmall/test/u0000000000242/chip/22/COL66.jpg",
+                    "skuPic_285": "/hmall/test/u0000000000242/sku/285/COL66.jpg",
+                    "skuPic_561": "/hmall/test/u0000000000242/sku/561/COL66.jpg",
+                },
+                "u0000000000242007": {
+                    "skuPic_40": "/hmall/test/u0000000000242/sku/40/COL69.jpg",
+                    "skuPic_1000": "/hmall/test/u0000000000242/sku/1000/COL69.jpg",
+                    "chipPic_22": "/hmall/test/u0000000000242/chip/22/COL69.jpg",
+                    "skuPic_285": "/hmall/test/u0000000000242/sku/285/COL69.jpg",
+                    "skuPic_561": "/hmall/test/u0000000000242/sku/561/COL69.jpg",
+                },
+                "u0000000000242002": {
+                    "skuPic_40": "/hmall/test/u0000000000242/sku/40/COL66.jpg",
+                    "skuPic_1000": "/hmall/test/u0000000000242/sku/1000/COL66.jpg",
+                    "chipPic_22": "/hmall/test/u0000000000242/chip/22/COL66.jpg",
+                    "skuPic_285": "/hmall/test/u0000000000242/sku/285/COL66.jpg",
+                    "skuPic_561": "/hmall/test/u0000000000242/sku/561/COL66.jpg",
+                },
+                "u0000000000242001": {
+                    "skuPic_40": "/hmall/test/u0000000000242/sku/40/COL66.jpg",
+                    "skuPic_1000": "/hmall/test/u0000000000242/sku/1000/COL66.jpg",
+                    "chipPic_22": "/hmall/test/u0000000000242/chip/22/COL66.jpg",
+                    "skuPic_285": "/hmall/test/u0000000000242/sku/285/COL66.jpg",
+                    "skuPic_561": "/hmall/test/u0000000000242/sku/561/COL66.jpg",
+                },
+                "u0000000000242000": {
+                    "skuPic_40": "/hmall/test/u0000000000242/sku/40/COL66.jpg",
+                    "skuPic_1000": "/hmall/test/u0000000000242/sku/1000/COL66.jpg",
+                    "chipPic_22": "/hmall/test/u0000000000242/chip/22/COL66.jpg",
+                    "skuPic_285": "/hmall/test/u0000000000242/sku/285/COL66.jpg",
+                    "skuPic_561": "/hmall/test/u0000000000242/sku/561/COL66.jpg",
+                },
+            },
+            "modelPicH5": [],
+            "colorList": [
+                {
+                    "chipPic": "/hmall/test/u0000000000242/chip/22/COL66.jpg",
+                    "styleText": "66 BLUE",
+                    "style": "BLUE",
+                    "colorNo": "COL66",
+                },
+                {
+                    "chipPic": "/hmall/test/u0000000000242/chip/22/COL69.jpg",
+                    "styleText": "69 NAVY",
+                    "style": "NAVY",
+                    "colorNo": "COL69",
+                },
+            ],
+            "main561": [
+                "/hmall/test/u0000000000242/main/first/561/1.jpg",
+                "/hmall/test/u0000000000242/main/other1/480/2.jpg",
+                "/hmall/test/u0000000000242/main/other2/480/3.jpg",
+                "/hmall/test/u0000000000242/main/other3/480/4.jpg",
+                "/hmall/test/u0000000000242/main/other4/480/5.jpg",
+                "/hmall/test/u0000000000242/main/other5/480/6.jpg",
+                "/hmall/test/u0000000000242/main/other6/480/7.jpg",
+                "/hmall/test/u0000000000242/main/other7/480/8.jpg",
+                "/hmall/test/u0000000000242/main/other8/480/9.jpg",
+                "/hmall/test/u0000000000242/main/other9/480/10.jpg",
+                "/hmall/test/u0000000000242/main/other10/480/11.jpg",
+                "/hmall/test/u0000000000242/main/other11/480/12.jpg",
+                "/hmall/test/u0000000000242/main/other12/480/13.jpg",
+                "/hmall/test/u0000000000242/main/other13/480/14.jpg",
+            ],
+            "mDetailPic": [],
+            "productCode": "u0000000000242",
+            "modelPicPC": {},
+            "main1000": [
+                "/hmall/test/u0000000000242/main/first/1000/1.jpg",
+                "/hmall/test/u0000000000242/main/other1/1000/2.jpg",
+                "/hmall/test/u0000000000242/main/other2/1000/3.jpg",
+                "/hmall/test/u0000000000242/main/other3/1000/4.jpg",
+                "/hmall/test/u0000000000242/main/other4/1000/5.jpg",
+                "/hmall/test/u0000000000242/main/other5/1000/6.jpg",
+                "/hmall/test/u0000000000242/main/other6/1000/7.jpg",
+                "/hmall/test/u0000000000242/main/other7/1000/8.jpg",
+                "/hmall/test/u0000000000242/main/other8/1000/9.jpg",
+                "/hmall/test/u0000000000242/main/other9/1000/10.jpg",
+                "/hmall/test/u0000000000242/main/other10/1000/11.jpg",
+                "/hmall/test/u0000000000242/main/other11/1000/12.jpg",
+                "/hmall/test/u0000000000242/main/other12/1000/13.jpg",
+                "/hmall/test/u0000000000242/main/other13/1000/14.jpg",
+            ],
+            "main80": [
+                "/hmall/test/u0000000000242/main/first/80/1.jpg",
+                "/hmall/test/u0000000000242/main/other1/80/2.jpg",
+                "/hmall/test/u0000000000242/main/other2/80/3.jpg",
+                "/hmall/test/u0000000000242/main/other3/80/4.jpg",
+                "/hmall/test/u0000000000242/main/other4/80/5.jpg",
+                "/hmall/test/u0000000000242/main/other5/80/6.jpg",
+                "/hmall/test/u0000000000242/main/other6/80/7.jpg",
+                "/hmall/test/u0000000000242/main/other7/80/8.jpg",
+                "/hmall/test/u0000000000242/main/other8/80/9.jpg",
+                "/hmall/test/u0000000000242/main/other9/80/10.jpg",
+                "/hmall/test/u0000000000242/main/other10/80/11.jpg",
+                "/hmall/test/u0000000000242/main/other11/80/12.jpg",
+                "/hmall/test/u0000000000242/main/other12/80/13.jpg",
+                "/hmall/test/u0000000000242/main/other13/80/14.jpg",
+            ],
+            "semi": {},
+            "detailPic": [],
+            "mMeasurementPic": ["/hmall/test/u0000000000242/measurement/1.jpg"],
+        }
+
 
 class TestUqRetriever(unittest.TestCase):
     def test_get_product_info_should_use_product_spu_url(self):
@@ -201,6 +334,17 @@ class TestUqRetriever(unittest.TestCase):
 
         session.get.assert_called_once_with(
             "https://d.uniqlo.com/tw/p/product/i/product/spu/pc/query/product_code/zh_TW",
+            headers=retriever._custom_headers,
+        )
+
+    def test_get_image_info_with_correct_url(self):
+        session = mock.MagicMock()
+
+        retriever = UqRetriever("u0000000000242", session)
+        retriever.get_image_info()
+
+        session.get.assert_called_once_with(
+            "https://www.uniqlo.com/tw/data/products/zh_TW/u0000000000242.json",
             headers=retriever._custom_headers,
         )
 
@@ -229,6 +373,19 @@ class TestUqRetriever(unittest.TestCase):
 
         with self.assertRaises(UqProductException):
             retriever.get_price_info()
+
+    def test_get_image_info_should_raise_uq_product_exception_when_request_not_succeeded(
+        self,
+    ):
+        session = mock.MagicMock()
+        response = mock.MagicMock()
+        response.ok = False
+        session.get.return_value = response
+
+        retriever = UqRetriever("product_code", session)
+
+        with self.assertRaises(UqProductException):
+            retriever.get_image_info()
 
 
 class TestUqProduct(unittest.TestCase):
@@ -263,4 +420,12 @@ class TestUqProduct(unittest.TestCase):
         self.assertEqual(
             "https://www.uniqlo.com/tw/zh_TW/product-detail.html?productCode=u0000000000242",
             item.website_url,
+        )
+
+    def test_uq_product_to_get_correct_image_url(self):
+        retriever = MockUqRetriever()
+        item = UqProduct(retriever)
+        self.assertEqual(
+            "https://www.uniqlo.com/tw/hmall/test/u0000000000242/main/first/561/1.jpg",
+            item.image_url,
         )
