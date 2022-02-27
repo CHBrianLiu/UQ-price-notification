0055a1e276e0578e536f017533915b0f5a1fcafa
brianliu <hong86323@gmail.com>
2022-02-27 10:14:46 +0800

    Add url parser unit tests.

--- tests/ut/shared/uq/test_uq_url_utils.py
+++ tests/ut/shared/uq/test_uq_url_utils.py
@@ -9,20 +9,32 @@ class TestUqProductCodeParser(unittest.TestCase):
         result = parser.get_product_code_from_url(url)
         self.assertEqual(result, "u0000000009993")
 
-    def test_get_correct_product_code_from_desktop_url(self):
+    def test_get_correct_product_code_from_mobile_url(self):
         url = "https://m.uniqlo.com/tw/product?pid=u0000000009993"
         parser = UqProductCodeParser()
         result = parser.get_product_code_from_url(url)
         self.assertEqual(result, "u0000000009993")
 
     def test_get_none_from_a_url_without_product_code(self):
-        url = ""
+        url = "https://www.uniqlo.com/tw/zh_TW/"
         parser = UqProductCodeParser()
         result = parser.get_product_code_from_url(url)
         self.assertEqual(result, None)
 
     def test_get_none_from_empty_url(self):
         url = ""
+        parser = UqProductCodeParser()
+        result = parser.get_product_code_from_url(url)
+        self.assertEqual(result, None)
+
+    def test_get_correct_product_code_from_multiple_parameter_url(self):
+        url = "https://www.uniqlo.com/tw/zh_TW/product-detail.html?foo=3&productCode=u0000000009993&bar=1"
+        parser = UqProductCodeParser()
+        result = parser.get_product_code_from_url(url)
+        self.assertEqual(result, "u0000000009993")
+
+    def test_get_none_from_non_sense_string(self):
+        url = "u1234"
         parser = UqProductCodeParser()
         result = parser.get_product_code_from_url(url)
         self.assertEqual(result, None)
