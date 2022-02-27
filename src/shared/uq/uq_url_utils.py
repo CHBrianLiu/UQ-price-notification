a5ab91391b929498f2d15acdeb75d4c66f402977
brianliu <hong86323@gmail.com>
2022-02-27 11:20:07 +0800

    Add parsing logic and fix tests.

--- src/shared/uq/uq_url_utils.py
+++ src/shared/uq/uq_url_utils.py
@@ -6,10 +6,15 @@ class UqProductCodeParser:
     A class with can parse product code from UNIQLO url
     """
 
-    def __init__(self):
-        self.pattern = re.compile(r"u[0-9]+")
+    MOBILE_URL_PATTERN = (
+        r"^https:\/\/m\.uniqlo\.com\/tw\/product\?.*pid=(?P<code>[A-Z|a-z|0-9]+)"
+    )
+    DESKTOP_URL_PATTERN = r"^https:\/\/www\.uniqlo\.com\/tw\/zh_TW\/product-detail.html\?.*productCode=(?P<code>[A-Z|a-z|0-9]+)"
 
-    def get_product_code_from_url(self, url: str) -> str | None:
+    def __init__(self, url: str):
+        self._url = url
+
+    def get_product_code_from_url(self) -> str | None:
         """
         Both desktop and mobile version urls are supported.
         Desktop url
@@ -17,5 +22,14 @@ class UqProductCodeParser:
         Mobile url
         https://m.uniqlo.com/tw/product?pid=u0000000009993
         """
-        match = self.pattern.search(url)
-        return match.group() if match else None
+        product_code = self._get_product_code_from_desktop_url()
+        product_code = product_code or self._get_product_code_from_mobile_url()
+        return product_code
+
+    def _get_product_code_from_mobile_url(self) -> str | None:
+        result = re.match(self.MOBILE_URL_PATTERN, self._url)
+        return result.group("code") if result is not None else None
+
+    def _get_product_code_from_desktop_url(self) -> str | None:
+        result = re.match(self.DESKTOP_URL_PATTERN, self._url)
+        return result.group("code") if result is not None else None
