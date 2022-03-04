269cb96a5062cf65cd4e32cddf4ebbf21c5832fe
brianliu <hong86323@gmail.com>
2022-03-04 18:27:51 +0800

    Migrate text message event handler to new mechanism

--- src/app/event_handlers.py
+++ src/app/event_handlers.py
@@ -1,18 +1,8 @@
 import linebot
 from linebot.models import MessageEvent, TextMessage, PostbackEvent
-import requests
 
-from src.shared.line.message_creators.base_creators import LineMessageCreator
-from src.shared.line.message_creators.template_message_creators import (
-    UqProductSubscriptionConfirmationMessageCreator,
-)
-from src.shared.line.message_creators.basic_message_creators import (
-    HelpMessageCreator,
-    ProductUrlErrorMessageCreator,
-)
 from src import config
-from src.shared.uq.uq_url_utils import UqProductCodeParser
-from src.shared.uq import uq_product
+from src.app.text_handlers import text_message_dispatcher
 from src.app.postback_handlers import postback_dispatcher
 
 line_api = linebot.LineBotApi(config.LINE_CHANNEL_TOKEN, config.LINE_API_ENDPOINT)
@@ -21,30 +11,17 @@ webhook_handler = linebot.WebhookHandler(config.LINE_CHANNEL_SECRET)
 
 @webhook_handler.add(MessageEvent, message=TextMessage)
 def handle_text_message_event(event: MessageEvent):
+    # Currently, only normal users can interact with our bot.
+    if event.source.type != "user":
+        return
+
     reply_token = event.reply_token
     message = event.message.text
+    user_id = event.source.user_id
 
-    if product_code := UqProductCodeParser(message).get_product_code_from_url():
-        reply_message = generate_response_for_given_product_code(product_code)
-    else:
-        reply_message = HelpMessageCreator().generate()
+    reply_message = text_message_dispatcher.run_task(message, user_id)
 
     line_api.reply_message(reply_token, reply_message)
-
-
-def generate_response_for_given_product_code(product_code: str) -> LineMessageCreator:
-    try:
-        with requests.Session() as session:
-            retriever = uq_product.UqRetriever(product_code, session)
-            product = uq_product.UqProduct(retriever)
-            message = UqProductSubscriptionConfirmationMessageCreator(
-                product
-            ).generate()
-
-    except uq_product.UqProductException:
-        message = ProductUrlErrorMessageCreator().generate()
-
-    return message
 
 
 @webhook_handler.add(PostbackEvent)
