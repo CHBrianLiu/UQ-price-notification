1dde97815a72fb51da02675b77bf889488bf2531
Chunhung (Brian) Liu <hong86323@gmail.com>
2022-03-06 13:26:07 +0800

    Add product management command support (#30)
    * Add NoSubscribedProductMessageCreator class
    
    * Add product management text handler.

--- src/app/text_handlers/text_handlers.py
+++ src/app/text_handlers/text_handlers.py
@@ -8,12 +8,16 @@ from src.app.text_handlers.text_dispatcher import (
 from src.shared.line.message_creators.basic_message_creators import (
     HelpMessageCreator,
     ProductUrlErrorMessageCreator,
+    NoSubscribedProductMessageCreator,
 )
 from src.shared.line.message_creators.template_message_creators import (
     UqProductSubscriptionConfirmationMessageCreator,
+    UqProductManagementTemplateMessageCreator,
 )
 from src.shared.uq.uq_url_utils import UqProductCodeParser
 from src.shared.uq.uq_product import UqRetriever, UqProduct, UqProductException
+from src.shared.db.utils import UserDataRetriever
+from src.shared.db.models import User, Product
 
 text_message_dispatcher = TextMessageDispatcher()
 
@@ -61,3 +65,26 @@ class ProductUrlTextMessageHandler(TextMessageHandlerBase):
             message = ProductUrlErrorMessageCreator().generate()
 
         return message
+
+
+@text_message_dispatcher.add
+class ProductListTextMessageHandler(TextMessageHandlerBase):
+    @staticmethod
+    def is_command(message: str) -> bool:
+        return message == "管理個人清單"
+
+    def execute(self) -> models.Message:
+        (user, _) = User.get_or_create(id=self._source_user_id, role_id=1)
+        products = UserDataRetriever(user).get_subscribed_products()
+
+        if not products:
+            return NoSubscribedProductMessageCreator().generate()
+
+        return self._generate_message_from_product_list(products)
+
+    def _generate_message_from_product_list(self, products: dict[str, Product]):
+        with requests.Session() as session:
+            uq_products = [
+                UqProduct(UqRetriever(code, session)) for code in products.keys()
+            ]
+            return UqProductManagementTemplateMessageCreator(uq_products).generate()
--- tests/ut/app/text_handlers/test_text_handlers.py
+++ tests/ut/app/text_handlers/test_text_handlers.py
@@ -6,6 +6,7 @@ from linebot import models
 from src.app.text_handlers.text_handlers import (
     DefaultTextMessageHandler,
     ProductUrlTextMessageHandler,
+    ProductListTextMessageHandler,
 )
 from src.shared.uq.uq_product import UqProductException
 
@@ -67,10 +68,78 @@ class TestProductUrlTextMessageHandler(unittest.TestCase):
     def test_execute_should_return_product_code_error_message_if_product_not_found(
         self, mock_requests, mock_retriever, mock_product
     ):
-
         handler = ProductUrlTextMessageHandler(
             "https://m.uniqlo.com/tw/product?pid=nothing", "user_id"
         )
         message = handler.execute()
 
         self.assertIsInstance(message, models.TextMessage)
+
+    class TestProductListTextMessageHandler(unittest.TestCase):
+        def test_is_command_should_return_true_if_matches_condition(self):
+            result = ProductListTextMessageHandler.is_command("管理個人清單")
+
+            self.assertEqual(result, True)
+
+        def test_is_command_should_return_false_if_not_matches_condition(self):
+            result = ProductListTextMessageHandler.is_command("Not matched")
+
+            self.assertEqual(result, False)
+
+        @mock.patch(
+            "src.app.text_handlers.text_handlers.UserDataRetriever", autospec=True
+        )
+        @mock.patch("src.app.text_handlers.text_handlers.User", autospec=True)
+        def test_is_command_should_return_no_subscribed_product_message_if_no_product(
+            self, mock_user, mock_user_data_retriever
+        ):
+            user = mock.MagicMock()
+            mock_user.get_or_create.return_value = (user, False)
+            user_data_retriever = mock_user_data_retriever.return_value
+            user_data_retriever.get_subscribed_products = {}
+
+            handler = ProductListTextMessageHandler("管理個人清單", "user_id")
+            result = handler.execute()
+
+            self.assertEqual(result.text, "你沒有追蹤中的商品喔！")
+
+        @mock.patch("src.app.text_handlers.text_handlers.UqProduct", autospec=True)
+        @mock.patch("src.app.text_handlers.text_handlers.UqRetriever", autospec=True)
+        @mock.patch("requests.Session", autospec=True)
+        @mock.patch(
+            "src.app.text_handlers.text_handlers.UserDataRetriever", autospec=True
+        )
+        @mock.patch("src.app.text_handlers.text_handlers.User", autospec=True)
+        def test_is_command_should_return_product_management_message(
+            self,
+            mock_user,
+            mock_user_data_retriever,
+            mock_session,
+            mock_uq_retriever,
+            mock_uq_product,
+        ):
+            user = mock.MagicMock()
+            mock_user.get_or_create.return_value = (user, False)
+            user_data_retriever = mock_user_data_retriever.return_value
+            user_data_retriever.get_subscribed_products = {
+                "product1": mock.MagicMock(),
+                "product2": mock.MagicMock(),
+            }
+            uq_product1 = mock.MagicMock()
+            uq_product1.name = "product1"
+            uq_product1.is_on_sale = True
+            uq_product1.original_price = 100
+            uq_product1.special_offer = 50
+            uq_product1.website_url = "https://whatever.com"
+            uq_product2 = mock.MagicMock()
+            uq_product2.name = "product2"
+            uq_product2.is_on_sale = False
+            uq_product2.original_price = 100
+            uq_product2.special_offer = 100
+            uq_product2.website_url = "https://whatever.com"
+            mock_uq_product.reset_mock(side_effect=[uq_product1, uq_product2])
+
+            handler = ProductListTextMessageHandler("管理個人清單", "user_id")
+            result = handler.execute()
+
+            self.assertIsInstance(result, models.TemplateSendMessage)
